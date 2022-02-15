654db4e5464e37c606f144bd548a47d8142977ef
Chintan Shah <chintanshah@promactinfo.com>
2022-02-15 14:54:17 +0530

    removes cypress from library project and adds eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jasmine: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "plugin:jasmine/recommended",
+        "eslint-plugin-import",
+        "eslint-plugin-eslint-comments",
+        "eslint-plugin-promise"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {},
+    overrides: [
+        {
+            files: ["*.spec.ts"],
+            plugins: ["jasmine"],
+            rules: {
+                "@typescript-eslint/unbound-method": "off",
+                MicrosoftEdge: "off",
+                "axe/forms": "off",
+            },
+        },
+    ],
+};
